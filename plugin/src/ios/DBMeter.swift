1d9a53fdaf33e599d2ed1c3dca2ed2092afd90f8
Alex Voitov <aleksvoj@gmail.com>
2018-10-15 17:31:19 +0300

    decrease interval

--- plugin/src/ios/DBMeter.swift
+++ plugin/src/ios/DBMeter.swift
@@ -54,7 +54,7 @@ import AVFoundation
 
             if (!self.isTimerExists) {
                 self.timer = DispatchSource.makeTimerSource(queue: DispatchQueue.main)
-                self.timer.scheduleRepeating(deadline: .now(), interval: DispatchTimeInterval.milliseconds(50))
+                self.timer.scheduleRepeating(deadline: .now(), interval: DispatchTimeInterval.milliseconds(10))
                 self.timer.setEventHandler(handler: self.timerCallBack)
                 self.isTimerExists = true
             }
